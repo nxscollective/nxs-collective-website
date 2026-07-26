fab57251eba2959131cc875ee35ec37dd29c638f
nxscollective <contact@nxscollective.net>
2026-07-26 12:27:56 +0800

    Publish new Trustindex widget

--- nxs-collective/src/components/home/GoogleReviews.tsx
+++ nxs-collective/src/components/home/GoogleReviews.tsx
@@ -1,3 +1,42 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { Container } from "@/components/ui/Container";
+
+const TRUSTINDEX_SRC =
+  "https://cdn.trustindex.io/loader.js?f49097077c557410b1660218e5c";
+
 export default function GoogleReviews() {
-  return null;
+  const widgetContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const container = widgetContainer.current;
+
+    if (!container) return;
+
+    container.innerHTML = "";
+
+    const script = document.createElement("script");
+    script.src = TRUSTINDEX_SRC;
+    script.async = true;
+    script.defer = true;
+
+    container.appendChild(script);
+
+    return () => {
+      script.remove();
+      container.innerHTML = "";
+    };
+  }, []);
+
+  return (
+    <section className="border-b border-black/10 bg-[#f1efe9] py-8">
+      <Container>
+        <div
+          ref={widgetContainer}
+          className="w-full [&_iframe]:!max-w-full [&_iframe]:!w-full [&_*]:max-w-full"
+        />
+      </Container>
+    </section>
+  );
 }
